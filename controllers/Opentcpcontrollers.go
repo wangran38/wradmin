029ba6ec673c3ef78f579657af29ad8b466136fb
unknown <wangrantian@qq.com>
2023-02-08 15:52:11 +0800

    新增了物联网的一些模拟数据

--- controllers/Opentcpcontrollers.go
+++ controllers/Opentcpcontrollers.go
@@ -9,47 +9,59 @@ import (
 )
 
 func Opentcp(c *gin.Context) {
-	listen, err := net.Listen("tcp", "127.0.0.1:20000")
-	if err != nil {
-		c.JSON(200, gin.H{
-			"code": 200,
-			"msg":  "操作失败！",
-		})
-		return
-	} else {
-		c.JSON(200, gin.H{
-			"code":    200,
-			"message": "服务器端口已开启！",
-			// "data":    ,
-		})
-		for {
-			conn, err := listen.Accept() // 建立连接
-			if err != nil {
-				fmt.Println("accept failed, err:", err)
-				continue
-			}
-			go process(conn) // 启动一个goroutine处理连接
-		}
+	c.JSON(200, gin.H{
+		"code":    200,
+		"message": "开启tcp！",
+		// "data":    ,
+	})
+	go Open()
+	// listen, err := net.Listen("tcp", "127.0.0.1:20000")
+	// if err != nil {
+	// 	c.JSON(200, gin.H{
+	// 		"code": 200,
+	// 		"msg":  "操作失败！",
+	// 	})
+	// 	return
+	// } else {
+	// 	fmt.Println("已经开启tcp端口成功！")
+	// 	// Okbeng(c)
+	// 	for {
+	// 		conn, err := listen.Accept() // 建立连接
+	// 		if err != nil {
+	// 			fmt.Println("accept failed, err:", err)
+	// 			continue
+	// 		}
+	// 		go process(conn) // 启动一个goroutine处理连接
+	// 	}
 
-	}
+	// }
 
 }
 
-// func Open() {
-// 	listen, err := net.Listen("tcp", "127.0.0.1:20000")
-// 	if err != nil {
-// 		fmt.Println("listen failed, err:", err)
-// 		return
-// 	}
-// 	for {
-// 		conn, err := listen.Accept() // 建立连接
-// 		if err != nil {
-// 			fmt.Println("accept failed, err:", err)
-// 			continue
-// 		}
-// 		go process(conn) // 启动一个goroutine处理连接
-// 	}
+// func Okbeng(c *gin.Context) {
+// 	go c.JSON(200, gin.H{
+// 		"code":    200,
+// 		"message": "服务器端口已开启！",
+// 		// "data":    ,
+// 	})
+
 // }
+
+func Open() {
+	listen, err := net.Listen("tcp", "127.0.0.1:20000")
+	if err != nil {
+		fmt.Println("listen failed, err:", err)
+		return
+	}
+	for {
+		conn, err := listen.Accept() // 建立连接
+		if err != nil {
+			fmt.Println("accept failed, err:", err)
+			continue
+		}
+		go process(conn) // 启动一个goroutine处理连接
+	}
+}
 
 func process(conn net.Conn) {
 	defer conn.Close() // 关闭连接
